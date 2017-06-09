7382d5b79c54a02555be1e7ac42b54e323d23623
Matt Raines <mattraines.nz@gmail.com>
2017-06-09 14:36:43 +1200

    save me

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -4,20 +4,72 @@ import {connect} from 'react-redux'
 
 import {getSkuxxes} from '../actions/game'
 
-const renderSkuxx = (skuxx) => (
-  <img key={skuxx.id} src={skuxx.img_url}/>
-)
+class Skuxxes extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {pairIndex: 0, skuxxes1: props.skuxxes1, skuxxes2: props.skuxxes2}
+  }
 
-const Skuxxes = ({skuxxes, dispatch}) => {
+componentDidMount() {
+  this.props.dispatch(getSkuxxes())
+}
+
+selectSkuxx() { //deal with counter.
+  
+this.tickSkuxx()
+}
+
+renderSkuxx (skuxx) {
+  return <span>
+    <img src={skuxx.img_url} width = '400' onClick={() => this.selectSkuxx()}/>
+  </span>
+}
+
+renderSkuxxes (idx) {
   return (
-  <div>
-    <button onClick={() => dispatch(getSkuxxes())}>Show Skuxxes</button>
-    {skuxxes.map(renderSkuxx)}
-  </div>
-)}
+    <div>
+      {this.renderSkuxx(this.state.skuxxes1[idx])}
+      {this.renderSkuxx(this.state.skuxxes2[idx])}
+    </div>
+  )
+}
+
+componentWillReceiveProps (props) {
+    let {skuxxes1, skuxxes2} = props
+    this.setState({skuxxes1, skuxxes2})
+}
+
+tickSkuxx () {
+  this.setState({pairIndex: this.state.pairIndex + 1})
+}
+
+render() {
+  return (
+    <div>
+      {this.state.skuxxes1.length > 0 ? this.renderSkuxxes(this.state.pairIndex) : ""}
+    </div>
+
+    )
+  }
+}
+
 
 const mapStateToProps = (state) => {
-return {skuxxes: state.game}
+  function sattoloCycle(arr) {
+    var items = arr.slice()
+    for (var i = items.length - 1; i >= 1; i--) {
+      var j = Math.floor(Math.random() * i)
+      var tmp = items[i]
+      items[i] = items[j]
+      items[j] = tmp
+    }
+    return items
+  }
+
+  var shuffled = sattoloCycle(state.game)
+  var shuffled2 = sattoloCycle(shuffled)
+
+  return {skuxxes1: shuffled, skuxxes2: shuffled2}
 
 }
 
